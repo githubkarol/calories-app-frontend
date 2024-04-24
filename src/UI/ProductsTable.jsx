a04fea23ebcdc33929c03ba7f55de0a9a6ef65a7
githubdevernest <github_ernest@onet.pl>
2024-04-24 10:51:14 +0200

    table with products mock added

---
+++ src/UI/ProductsTable.jsx
@@ -0,0 +1,48 @@
+import {useEffect, useState} from "react";
+
+const ProductsTable = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, [products]);
+    return (
+        <div>
+            <h1>Products Table</h1>
+            <table>
+                <thead>
+                <tr>
+                    {['Product ID', 'Name', 'Category', 'Uses', 'Energy', 'Carbohydrates', 'Fat', 'Protein'].map(header => (
+                        <th key={header}>{header}</th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {products.map(({ productId, name, category, uses, energy, carbohydrates, fat, protein }) => (
+                    <tr key={productId}>
+                        <td>{productId}</td>
+                        <td>{name}</td>
+                        <td>{category}</td>
+                        <td>
+                            <ul>
+                                {uses.map(use => (
+                                    <li key={use}>{use}</li>
+                                ))}
+                            </ul>
+                        </td>
+                        <td>{energy}</td>
+                        <td>{carbohydrates}</td>
+                        <td>{fat}</td>
+                        <td>{protein}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ProductsTable;
